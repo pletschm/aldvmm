1485008b603fc25a253d64d887d6b1170640367d
pletschm <markpletscher@gmx.ch>
2023-05-26 16:58:13 +0200

    - Make generic print function - make generic function coef

---
+++ R/coef.aldvmm.R
@@ -0,0 +1,21 @@
+#' Extract Model Coefficients of Adjusted Limited Dependent Variable Mixture Model Fits
+#'
+#' The generic function
+#' \ifelse{html}{\code{\link[stats]{coef}}}{\code{stats::coef()}} extracts the 
+#' vector of coefficients from an object of class "aldvmm"}.
+#'
+#' @param object an object inheriting from class 'aldvmm'.
+#' @param ... further arguments passed to or from other methods.
+#'
+#' @return a named vector of numeric parameter estimates.
+#'
+#' @method coef aldvmm
+#' @rdname coef
+#' @export coef.aldvmm
+#'
+#' @export
+
+coef.aldvmm <- function (object, 
+                         ...) {
+  object$coef
+}
--- R/print.aldvmm.R
+++ R/print.aldvmm.R
@@ -1,5 +1,21 @@
-print.aldvmm <- function(x, digits = max(3L, getOption("digits") - 3L), ...)
-{
+#' Print Adjusted Limited Dependent Variable Mixture Model Fits
+#'
+#' The generic function
+#' \ifelse{html}{\code{\link[stats]{coef}}}{\code{stats::print()}} prints key 
+#' attributes of an object of class "aldvmm"}.
+#'
+#' @param object an object inheriting from class 'aldvmm'.
+#' @param ... further arguments passed to or from other methods.
+#'
+#' @method print aldvmm
+#' @rdname print
+#' @export print.aldvmm
+#'
+#' @export
+
+print.aldvmm <- function(x, 
+                         digits = max(3L, getOption("digits") - 3L), 
+                         ...) {
   cat("\nCall:  ",
       paste(deparse(x$call), sep = "\n", collapse = "\n"), "\n\n", sep = "")
   if(length(coef(x))) {
@@ -11,8 +27,8 @@ print.aldvmm <- function(x, digits = max(3L, getOption("digits") - 3L), ...)
     print.default(format(x$coef, digits = digits),
                   print.gap = 2, quote = FALSE)
   } else cat("No coefficients\n\n")
-  cat("\nDegrees of Freedom:", unlist(x$df.null), "Total (i.e. Null); ",
-      unlist(x$df.residual), "Residual\n")
+  cat("\nDegrees of Freedom:", c(rbind(unlist(x$df.null), c(" (beta), ", " (delta), ", " (full)"))), "Total (i.e. Null);",
+      "\n                   ", c(rbind(unlist(x$df.residual), c(" (beta), ", " (delta)"))), "Residual\n")
   if(nzchar(mess <- naprint(x$na.action))) cat("  (",mess, ")\n", sep = "")
   cat("Log-likelihood:	   ",	format(signif(x$ll, digits)),
       "\nResidual Deviance:", format(signif(x$deviance, digits)),
