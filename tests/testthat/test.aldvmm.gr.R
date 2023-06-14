07a2b2c0edb13a45c8ec0e8dbeb2688a8616b688
pletschm <markpletscher@gmx.ch>
2023-06-14 11:53:19 +0200

    Updated likelihood function with simpler calculation from aldvmm.sc.

---
+++ tests/testthat/test.aldvmm.gr.R
@@ -0,0 +1,12 @@
+test_that('Check calculation of gradients of log-likelihood.', {
+
+  # Output is colSums of gradient matrix
+  #------------------------------------
+
+  # Numeric and finite values
+  #--------------------------
+  
+  # Zero values for infinite parameters
+  #------------------------------------
+  
+})
---
+++ tests/testthat/test.aldvmm.sc.R
@@ -0,0 +1,13 @@
+test_that('Check calculation of gradients of log-likelihood w.r.t. parameters per observation.', {
+
+  # Gradients are numeric and finite
+  #---------------------------------
+  
+  # Parameters are in same order as in initial values
+  #--------------------------------------------------
+
+  # Small differences from numDeriv:Jacobian results
+  #-------------------------------------------------
+  
+  
+})
