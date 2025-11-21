8c22fcb3276e8b4e8bb34ea0e6a89d5139c53e6a
pletschm <markpletscher@gmx.ch>
2025-11-21 21:37:35 +0100

    preparing rhub checks

--- prepare_submission.R
+++ prepare_submission.R
@@ -62,7 +62,7 @@ Sys.setenv(R_GSCMD = "C:/Users/phm1/gs10.00.0/bin/gswin64c.exe")
 # Save pdf vignette in vignettes folder
 #--------------------------------------
 
-rmarkdown::render("./vignettes/pdf_vignette.Rmd")
+#rmarkdown::render("./vignettes/pdf_vignette.Rmd")
 
 # Save html vignette in vignettes folder
 #--------------------------------------
@@ -86,7 +86,7 @@ devtools::build()
 # Source check in shell
 #----------------------
 
-# R CMD check --as-cran ../aldvmm_0.8.9.tar.gz
+# R CMD check --as-cran ../aldvmm_0.9.0.tar.gz
 
 # Winbuilder
 #------------
@@ -98,5 +98,7 @@ devtools::build()
 # rhub checks
 #------------
 
-rhub::check("../aldvmm_0.8.9.tar.gz", platform = "ubuntu-gcc-release")
-rhub::check("../aldvmm_0.8.9.tar.gz", platform = "debian-gcc-devel")
+rhub::list_platforms()
+rhub::rhub_check(path = "../aldvmm_0.9.0.tar.gz", platforms = "ubuntu-latest")
+rhub::check("../aldvmm_0.9.0.tar.gz", platform = "ubuntu-gcc-release")
+rhub::check("../aldvmm_0.9.0.tar.gz", platform = "debian-gcc-devel")
